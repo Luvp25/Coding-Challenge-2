9445c04efb9599f30ecd66c7a1ac7d4ebe16b5cc
Luv Patel <luvp98@gmail.com>
2026-02-01 22:03:40 -0500

    made the variables const and completed the formulas and console logs

--- PricingAssistant.js
+++ PricingAssistant.js
@@ -1,5 +1,19 @@
-const costPerUnit;
-const basePrice;
-const discountRate;
-const salesTaxRate;
-const fixedMonthlyCosts;
+let productName = "Droot's Donuts"; //Product name is droot's donuts
+const costPerUnit = 1.59;
+const basePrice = 2.59;
+const discountRate = 0.1;
+const salesTaxRate = 0.08;
+const fixedMonthlyCosts = 340;
+
+let discountedPrice = basePrice *(1-discountRate);
+let finalPriceWithTax = discountedPrice* (1+salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
+
+console.log("Product Name: ", productName);
+console.log("Discounted Tax: ", discountedPrice);
+console.log("Final Price with Tax: ", finalPriceWithTax);
+console.log("Profit per unit: ", profitPerUnit);
+console.log("Break-Even units: ", breakEvenUnits);
+console.log("Per-unit profitability: ", isProfitablePerUnit);
